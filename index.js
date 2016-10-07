2fefbd78bd3d80c67c385b38dac4b0680b04e0e2
Sylvain Vriens <info@sylvain.nl>
2016-10-07 11:35:14 +0200

    basic view setup

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 "use strict";
 
 console.log('** YOUR WEEKLY PRESIDENT**');
+
 const VideoPlayer = require('./VideoPlayer');
 
-var videoPlayer = new VideoPlayer();
+try{
+  const {app, BrowserWindow} = require('electron')
+
+  app.on('ready', ()=>{
+    let win = new BrowserWindow({width: 1920, height: 1080, useContentSize: true});
+    win.setMenu(null);
+    win.loadURL(`file://${__dirname}/html/index.html`);
+
+    win.webContents.openDevTools()
+
+  });
+
+  app.on('window-all-closed', ()=>{
+    app.quit();
+  });
+
+  //open window etc
+  let videoPlayer = new VideoPlayer();
+}catch(e){
+  let videoPlayer = new VideoPlayer();
+}
