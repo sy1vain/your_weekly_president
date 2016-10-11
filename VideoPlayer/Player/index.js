a12f4e6ee5bc9ab638455d976611dabe2cfd669a
Sylvain Vriens <info@sylvain.nl>
2016-10-11 15:29:59 +0200

    replaced player (fingers crossed)

--- VideoPlayer/Player/index.js
+++ VideoPlayer/Player/index.js
@@ -1,9 +1,7 @@
 "use strict"
 
-const omx = require('omx-interface');
+const OMXPlayer = require('omxplayer');
 const path = require('path');
-
-const IGNORE_STATUS_UPDATES_ON_START = 1;
 
 class Player {
 
@@ -14,11 +12,12 @@ class Player {
     this._startTime = 0;
     this._ignoreStatusUpdates = 0;
 
-    this._filepath = path.join(__dirname, '..', '..', 'tmp', 'ywp.mp4');
-
-    omx.onProgress((status)=>{
-      this._handleStatus(status, false);
+    this.player = new OMXPlayer();
+    this.player.on('error', (err)=>{
+      //catching error but ignoring it
     });
+
+    this._filepath = path.join(__dirname, '..', '..', 'tmp', 'ywp.mp4');
   }
 
   play(position=0){
@@ -31,62 +30,54 @@ class Player {
     return (Date.now() - this._startTime)/1000;
   }
 
+  set time(seconds){
+    if(isNaN(seconds)) return;
+    this._startTime = Date.now() - 1000*seconds;
+  }
+
   get playing(){
     return this._playing;
   }
 
   seekTo(position){
     position = position || 0;
-    if(Math.abs(position-this.time)<1 && this.playing) return;
 
     clearTimeout(this._startDelayer);
-    //mark as we are here
-    this._handleStatus({position}, true);
 
-    if(this._stopOnSeek){
-      console.log('stop');
-      omx.stop();
-      this._playing = false;
-    }
+    this.player.getPosition((err, seconds)=>{
+      if(err){
+        this._startPlayer(position, this._startDelay);
+        return;
+      }
+      if(Math.abs(seconds-position)<1) return; //close enough
 
-    if(this.playing){
-      console.log('live seek');
-      this._handleStatus({position}, true);
-      omx.setPosition(position);
-    }else{
-      //delayed start in case we get another scrub
-      this._startDelayer = setTimeout(()=>{
-
-        this._playing = true;
-        // !! we use position - 1 because there is an offset problem potentially
-        omx.open(this._filepath, {
-          startAt: position-1,
-          blackBackground: false,
-          disableKeys: true,
-          disableOnScreenDisplay: true
+      if(!this._stopOnSeek){
+        this.player.setPosition(position, (err, seconds)=>{
+          this.time = seconds;
         });
-        console.log('start omx (from stopped state?)', position);
-        //update internal position (again)
-        this._handleStatus({position}, true);
+        return;
+      }
 
-      }, this._startDelay);
-    }
+      this.time = position;
+      this._startPlayer(position, this._startDelay);
+    });
   }
 
-  _handleStatus(status, force){
-    if(!status) return;
-    if(!this._playing && !force) return;
-    if(force===false && this._ignoreStatusUpdates>0){
-      console.log('ignoring status update');
-      this._ignoreStatusUpdates--;
+  _startPlayer(pos=0, delay=0){
+    if(delay>0){
+      this._startDelayer = setTimeout(()=>{
+        this._startPlayer(pos);
+      });
       return;
     }
-    //we forced it, so the next IGNORE_STATUS_UPDATES_ON_START ones will be wrong
-    if(force===true){
-      this._ignoreStatusUpdates = IGNORE_STATUS_UPDATES_ON_START;
-    }
-    console.log('handle status', status, force);
-    this._startTime = Date.now() - status.position * 1000;
+
+    this.time = pos;
+
+    this.player.open(this._filepath, {
+      'no-osd': true,
+      'no-keys': true,
+      pos
+    });
   }
 
 }
